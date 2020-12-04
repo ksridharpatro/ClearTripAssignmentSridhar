96327d0da7e2fc790edec83a837bd7de2d6c33e5
sridhar.patro <sridhar.patro@happay.in>
2020-12-04 20:07:12 +0530

    Activity UI updated with network call.

--- app/src/main/java/com/example/cleartripassignmentsridhar/ui/MainActivity.kt
+++ app/src/main/java/com/example/cleartripassignmentsridhar/ui/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.example.cleartripassignmentsridhar.ui
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import com.example.cleartripassignmentsridhar.R
 import com.example.cleartripassignmentsridhar.repository.CountryRepository
+import com.example.cleartripassignmentsridhar.util.Status
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,6 +23,23 @@ class MainActivity : AppCompatActivity() {
             this,
             countryViewModelFactory
         ).get(CountryVieModel::class.java)
-
+        vieModel.countryResource.observe(this, {
+            when (it.status) {
+                Status.SUCCESS -> {
+                    progressBar.visibility = View.GONE
+                    groupUserDetails.visibility = View.VISIBLE
+                    Toast.makeText(this, it.data.toString(), Toast.LENGTH_SHORT).show()
+                }
+                Status.LOADING -> {
+                    progressBar.visibility = View.VISIBLE
+                    groupUserDetails.visibility = View.GONE
+                }
+                Status.ERROR -> {
+                    progressBar.visibility = View.GONE
+                    groupUserDetails.visibility = View.GONE
+                    Toast.makeText(this, it.message, Toast.LENGTH_SHORT).show()
+                }
+            }
+        })
     }
 }
