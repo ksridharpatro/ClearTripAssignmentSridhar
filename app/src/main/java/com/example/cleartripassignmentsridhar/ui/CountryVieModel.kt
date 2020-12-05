d94feacb9b7d0e4c6b4fc7a12e7b2732d43d51d8
sridhar.patro <sridhar.patro@happay.in>
2020-12-05 08:38:59 +0530

    validation message changed

--- app/src/main/java/com/example/cleartripassignmentsridhar/ui/CountryVieModel.kt
+++ app/src/main/java/com/example/cleartripassignmentsridhar/ui/CountryVieModel.kt
@@ -15,10 +15,10 @@ class CountryVieModel(
 
     fun validateUserDetails(country: Country, name: String, passport: String): String {
         if (InputField.NAME in country.mandatoryFields && name.isBlank()) {
-            return "Error: Name is mandatory"
+            return "Error: Name is invalid"
         }
         if (InputField.PASSPORT_NUMBER in country.mandatoryFields && passport.isBlank()) {
-            return "Error: Passport number is mandatory"
+            return "Error: Passport number invalid"
         }
         return "Validation Successful"
     }
